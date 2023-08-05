060979d129f46571f9a68e32dccfe7c7c29c51fb
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-08-04 22:09:56 -0300

    Add files via upload

---
+++ OperacoesBasicas/Entidades/Operacoes.java
@@ -0,0 +1,26 @@
+package Entidades;
+
+public class Operacoes {
+//Metodos
+	public float getAdicao(float number, float numero) {
+		return number+numero;}
+	
+	public float getSubtracao(float number, float numero) {
+		return number-numero;}
+	
+	public float getDivisao(float number, float numero) {
+		if (numero==0) {
+			System.out.println("Nao se pode dividir por zero");
+			return number;
+		}else {
+			return number/numero;}}
+	
+	public float getMultiplicacao(float number, float numero) {
+		return number*numero;}
+	
+	public float getRaiz(float number) {
+		return (float) Math.sqrt(number);}
+	
+	public float getPotencia(float number, float numero) {
+		return (float) Math.pow(number, numero);}	
+}
