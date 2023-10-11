220381eb92c41846b7d15a241d7045ab4391891f
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-10-11 08:14:01 -0300

    Add files via upload

---
+++ 2. AreaVolumeCilindro/Entidades/Cilindro.java
@@ -0,0 +1,29 @@
+package Entidades;
+
+/* Classes sao representacoes de coisas do mundo real, onde normalmente 
+ * declaramos atributos e metodos, que representam, respectivamente as 
+ * caracteristicas e comportamentos desse objeto */
+public class Cilindro {
+	private float raio;
+	private float altura;
+	
+/* Construtor, que torna obrigatorio as informacoes para referenciar esse objeto */
+	public Cilindro() {
+		this.raio = raio;
+		this.altura = altura;
+	}
+	
+//Metodos
+	//Em java, para alguns cálculos, usamos a biblioteca Math 
+	public float exibirArea(float raio, float altura) {
+		float area;
+		area = (float) (raio*Math.PI*2)*(raio+altura);
+		return area;
+	}
+	
+	public float exibirVolume(float raio, float altura) {
+		float volume;
+		volume = (float) (altura*Math.PI*Math.pow(raio,3));
+		return volume;
+	}	
+}
