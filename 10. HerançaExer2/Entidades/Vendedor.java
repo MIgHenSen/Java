6cb2058c6e4ab78980ed61df4ed963c4930af34f
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-10-13 10:48:40 -0300

    Add files via upload

---
+++ 10. HerançaExer2/Entidades/Vendedor.java
@@ -0,0 +1,26 @@
+package Entidades;
+
+/* Classes sao representacoes de coisas do mundo real, onde normalmente 
+ * declaramos atributos e metodos, que representam, respectivamente as 
+ * caracteristicas e comportamentos desse objeto */
+public class Vendedor extends Funcionario{
+	private float vendas;
+    
+/* Construtor que obriga informacoes para referenciar objeto */
+    public Vendedor(String nome, String rg, float salario, float vendas) {
+        super(rg, rg, vendas);
+        this.vendas = vendas;
+    }
+    
+//Metodos
+    public void acumularVendas(float valor) {
+        vendas = vendas + valor;
+    }
+    
+    @Override
+    public float calcularHolerite() {
+    	float salarioTotal = super.calcularHolerite();
+    	salarioTotal = (float) (salarioTotal + (vendas * 0.05));
+        return salarioTotal;
+    }
+}
