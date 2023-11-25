5c556d6bc5aa2eff9c034ba8daa6e9cf0673cc63
MIgHenSen <128144438+MIgHenSen@users.noreply.github.com>
2023-11-25 14:48:48 -0300

    Update Main.java

--- A. HerançaExer2/Principal/Main.java
+++ A. HerançaExer2/Principal/Main.java
@@ -1,29 +1,18 @@
 package Principal;
-
-/* Biblioteca (library) é uma coleção de subprogramas, que contem dados
- * auxiliares. Chamamos essas bibliotecas através dos imports */
 import Entidades.Administrador;
 import Entidades.Vendedor;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args){
-		// Declaracao de variaveis de tipo string(conjunto de caracteres)
 		String nome = null, rg = null;
-		// Declaracao de variaveis de tipo inteiro
 		int salario = 0, vendas = 0;
 		
-		// Declaracao de scanner, para que o codigo leia o que o usuario digitar
 		Scanner lerdados = new Scanner(System.in);
-		// Objeto criado para chamada de metodos
 		Vendedor vendedor = new Vendedor(nome, rg, salario, vendas);
-		// Objeto criado para chamada de metodos
 		Administrador administrador = new Administrador(nome, rg, salario);
 		
-		/* As clausulas try/catch garantem o tratamento de excecoes, codigos 
-		 * que nao sao totalmente atendidos e geram algum erro */
 		try {
 			System.out.println("Digite as informa��es do vendedor: ");
 			System.out.println("Nome: ");
@@ -59,7 +48,6 @@ public class Main {
 			vendas = lerdados.nextInt();
 			vendas = administrador.setSalario(salario);
 			
-		// Tratamento de excecoes
 		/* InputMismatchException indiga que um elemento solicitado nao existe, 
 		* ou seja, o usuario digitou algo que nao deveria */
 		}catch(InputMismatchException e) {
